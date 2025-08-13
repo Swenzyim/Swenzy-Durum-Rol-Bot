562ad6d561d75849f015359cdd39348d6685ee35
Swenzy <qnuran565@gmail.com>
2025-08-13 15:19:06 +0400

    Swenzy-Durum-Rol
    Basit Durum Botu.

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+ "TOKEN": 'bot-token', 
+ "GUILD_ID": 'sunucu-id',  
+ "ROLE_ID": 'rol-id',   
+ "EXPECTED_STATUS": 'Alinacak Durum', 
+ "LOG_CHANNEL_ID": 'kanal-id', 
+ "EMOJI": '👋', 
+ "STATUS": [
+    "discord.gg/siyahbeyaz",
+
+]
+ 
+  };
